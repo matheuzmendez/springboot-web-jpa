c34c3a8be1999de949b650b1f731e421847fab7e
Matheus Mendes <mendesm13.m@gmail.com>
2022-10-31 12:02:54 -0300

    Adiciona entidade category

--- src/main/java/com/mendes/project/config/TestConfig.java
+++ src/main/java/com/mendes/project/config/TestConfig.java
@@ -1,7 +1,6 @@
 package com.mendes.project.config;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.mendes.project.entities.Category;
 import com.mendes.project.entities.Order;
 import com.mendes.project.entities.User;
+import com.mendes.project.entities.enums.OrderStatus;
+import com.mendes.project.repositories.CategoryRepository;
 import com.mendes.project.repositories.OrderRepository;
 import com.mendes.project.repositories.UserRepository;
 
@@ -23,15 +25,25 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category oneCategory = new Category(null, "Electronics");
+		Category twoCategory = new Category(null, "Books");
+		Category threeCategory = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(oneCategory, twoCategory, threeCategory));
+		
 		User firstUser = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User secondUser = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
-		Order oneOrder = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), firstUser);
-		Order twoOrder = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), secondUser);
-		Order threeOrder = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), firstUser);
+		Order oneOrder = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, firstUser);
+		Order twoOrder = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, secondUser);
+		Order threeOrder = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, firstUser);
 		
 		userRepository.saveAll(Arrays.asList(firstUser, secondUser));
 		orderRepository.saveAll(Arrays.asList(oneOrder,twoOrder,threeOrder));
